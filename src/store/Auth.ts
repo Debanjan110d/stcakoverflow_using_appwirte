93dc5ebbc8f6fa8966f4cd7575ec392e29791223
Debanjan110d <debanjangamedu@gmail.com>
2025-11-12 23:53:14 +0530

    Refactor MyAuthStore interface to enhance user session management and add account creation functionality

--- src/store/Auth.ts
+++ src/store/Auth.ts
@@ -16,11 +16,50 @@ export interface UserPrefs {
     email: string
 }
 
-interface MyAuthStore {
+interface MyAuthStore { //? This is a interface ,its comin g up because we are using typescript if we are using hs we do't have to work with these things 
     session: Models.Session | null;// This is given by the appwrite itslf
     jwt : string | null;
     user : Models.User<UserPrefs> | null;
     hydrated: boolean;//it will depend on zustand 
     
+
+    sethydrated():void; // basically it will be true when the user is logged in and false when the user is logged out
+    verrifySession(): Promise<void>;
+    login(email: string, password: string): Promise<
+    {
+        sucess : boolean // appwirte 1O1
+        error? : AppwriteException | null; // Thre is a chance that there is no errors
+    }
+    >;
+    createAccount(
+        name:string,
+        email: string,
+        password: string,
+    ): Promise<
+    {
+        sucess : boolean 
+        error? : AppwriteException | null; 
+    }
+    >;
+
+    logout(): Promise<void>
+    
 }
+
+export const useAuthStore = create<MyAuthStore>()( //? the 1st () are initilizing the store and then we are immediately passing on a method onto this one and we will provide everything 
+    persist( //* We can go into the docs but I have already studied it on my way home form college so letts continue
+
+        immer(),
+        {
+            name: "auth",
+            onRehydrateStorage:()=>{
+                return(state,error)=>{// Here this is a call back and //? zustand provides me with the state so I do not have to explicitly declare it here
+                    if (!error) state?.sethydrated()
+                }
+            }
+        }
+
+    )
+
+)
 

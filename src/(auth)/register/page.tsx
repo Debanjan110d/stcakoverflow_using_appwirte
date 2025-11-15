f731ef24c1306edb0b1249c01a6451b0b7027d87
Debanjan110d <debanjangamedu@gmail.com>
2025-11-15 21:02:59 +0530

    Implement registration functionality with form validation and account creation

--- src/(auth)/register/page.tsx
+++ src/(auth)/register/page.tsx
@@ -1,6 +1,51 @@
-import React from 'react'
+import { useAuthStore } from '@/store/Auth'
+import { LogIn } from 'lucide-react';
+import React, { FormEvent, useState } from 'react'
 
 function registerPage() {
+    const {createAccount,login} = useAuthStore();
+    const [isLoading,setIsLoading] = useState(false);
+    const [error,setError] = useState(" ");
+
+
+    const handleSubmit = async (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        //collect data
+        const formData = new FormData(e.currentTarget);
+        const name = formData.get('name');
+        const email = formData.get('email');
+        const password = formData.get('password');
+;
+
+        //validation
+        if(!email || !password || !name){
+            setError('Please fill in all the fields');
+            setIsLoading(false);
+            return;
+        }
+
+        //call the store
+        setIsLoading(true);
+        setError("");
+
+
+        const response = await createAccount(email as string,password as string,name as string);
+        if (response.error) {
+            setError(()=> response.error!.message);
+            
+        } else {
+            const loginResponse = await login(email as string,password as string);
+            
+            if (loginResponse.error) {
+                setError(()=> loginResponse.error!.message);
+            }
+        }
+        setIsLoading(()=> false);
+
+    }
+    
     return (
         <div>registerPage</div>
     )
